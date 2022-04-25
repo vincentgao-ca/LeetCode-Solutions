b11493da26eb6f540f9e6e9450a96c2a5cd05674
vincentgao-ca <78761701+vincentgao-ca@users.noreply.github.com>
2022-04-24 22:56:50 -0700

    Update best-time-to-buy-and-sell-stock.cpp
    O(n) time and easy to understand. caculate up to ith day min and max, get the max profit at last

--- C++/best-time-to-buy-and-sell-stock.cpp
+++ C++/best-time-to-buy-and-sell-stock.cpp
@@ -19,3 +19,35 @@ public:
         return release1;
     }
 };
+
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        if (prices.empty())
+            return 0;
+        
+        int mini = prices[0];
+        int maxi = 0;
+        int n = prices.size();
+        
+        for(int i=0; i<n; i++) {
+            
+            /*  o(n2), time , no good
+            for(int j=i+1; j<n; j++) {
+                
+                maxi = max(maxi, prices[j]-prices[i]);
+                
+            }
+            */
+            
+            //o(n) time, better and easy to understand
+            mini = min(mini, prices[i]);
+            maxi = max(maxi, prices[i]-mini);
+            
+        }
+        
+        return maxi; 
+        
+    }
+};
